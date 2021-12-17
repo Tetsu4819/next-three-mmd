560d792870f15b3a095e0b087f6852f48e0775b1
tetsu4819 <tetsu4819@gmail.com>
2021-12-18 06:41:18 +0900

    :sparkles: load pmx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
+import { MMDCanvas } from '../components/MMD'
+
 export default function Home() {
-  return (
-    <h1 className="text-6xl text-blue-600 text-center my-16">HelloNext!</h1>
-  )
+  return <MMDCanvas />
 }
